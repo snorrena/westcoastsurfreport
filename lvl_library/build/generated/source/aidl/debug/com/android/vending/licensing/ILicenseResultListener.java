1522184d6fa6eaaefa84199c1c8accdf110264b7
Scott Norrena <snorrena@gmail.com>
2018-12-13 15:10:09 -0800

    committing local changes

--- lvl_library/build/generated/source/aidl/debug/com/android/vending/licensing/ILicenseResultListener.java
+++ lvl_library/build/generated/source/aidl/debug/com/android/vending/licensing/ILicenseResultListener.java
@@ -1,6 +1,6 @@
 /*
  * This file is auto-generated.  DO NOT MODIFY.
- * Original file: C:\\Users\\Admin\\Desktop\\Github\\Android\\Android Studio\\westcoastsurfreport-master\\lvl_library\\src\\main\\aidl\\com\\android\\vending\\licensing\\ILicenseResultListener.aidl
+ * Original file: C:\\Users\\snorr\\Desktop\\Github\\Android\\Android Studio\\westcoastsurfreport-master\\lvl_library\\src\\main\\aidl\\com\\android\\vending\\licensing\\ILicenseResultListener.aidl
  */
 package com.android.vending.licensing;
 public interface ILicenseResultListener extends android.os.IInterface
