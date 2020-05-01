a81a022fe2b75a24e7a96a3a559d7fbde4c31ab6
Scott Norrena <snorrena@gmail.com>
2020-04-30 18:51:49 -0700

    added code to scrap full 6 hours of data on start up

--- app/src/main/java/com/rsnorrena/westvansurfreport/parsers/JsoupWebScrape.java
+++ app/src/main/java/com/rsnorrena/westvansurfreport/parsers/JsoupWebScrape.java
@@ -60,15 +60,89 @@ public class JsoupWebScrape {
 
             if (doc != null) {//only attempt to parse if a document is returned.
 
+                String recordKey = null;
+                String date = null;
+                String time = null;
+                String winddirection = null;
+                String windspeed = null;
+                String waveheight = null;
+                String waveinterval = null;
+                String winddirectiondegrees = null;
+                String nameOfMonth = null;
+
+                //get the current report
+                Element currentReport = doc.getElementById("data");
+                Element currentReportTable = currentReport.select("table").get(0);
+                Elements currentReportRows = currentReportTable.select("tr");
+
+                Element titleDataHeader = currentReport.getElementsByClass("titleDataHeader").get(0);
+                String titleData = RssXMLParser.html2text(titleDataHeader.toString());//clean out the html tags
+                Log.d(TAG, "TitleDataHeader");
+                Log.d(TAG, titleData.toString());
+                String[] titleDataHeaderTokens = titleData.toString().split(" ");
+                Log.d(TAG, "Title token data");
+                for(int i = 0; i < titleDataHeaderTokens.length; i++){
+                    Log.d(TAG, "index: " + i + ": " + titleDataHeaderTokens[i]);
+                }
+
+                for(int i = 0; i < currentReportRows.size(); i++){
+                    Element row = currentReportRows.get(i);
+                    Elements cols = row.select("td");
+                    String cleanedRow = RssXMLParser.html2text(cols.toString());//clean out the html tags
+                    String demilms = "[ ]";
+                    String[] tokens = cleanedRow.split(demilms);
+                    //iterate through current data in table rows
+                    Log.d(TAG, "Row index: " + i);
+                    if(i == 1){
+                        winddirection = tokens[3];
+                        winddirectiondegrees = tokens[5];
+                    }
+                    if(i == 2){
+                        windspeed = tokens[3];
+                    }
+                    if(i==4){
+                        waveheight = tokens[3];
+                    }
+                    if(i==5){
+                        waveinterval = tokens[4];
+                    }
+                    for(String item: tokens) {
+                        Log.d(TAG, item);
+                    }
+
+                }
+
+                recordKey = setRecordKey(6);//set the record key for the current report
+                String numericTime = titleDataHeaderTokens[5].substring(1, titleDataHeaderTokens[5].length());
+                time = setReportTime(numericTime + " " + titleDataHeaderTokens[6]);
+                Log.d(TAG, "Record time: " + time);
+                String dateToParse = titleDataHeaderTokens[9];
+                String[] dateElements = dateToParse.split("/");
+                int month = Integer.valueOf(dateElements[0]);
+                nameOfMonth = getNameOfMonth(month);
+                Log.d(TAG, "Month int: " + month + ", Month name: " + nameOfMonth);
+                date = nameOfMonth + " " + dateElements[0] + ", " + dateElements[2].substring(0, (dateElements[2].length() - 1));
+                Log.d(TAG, "Date: " + date);
+
+                //the Halibut bank data items are added to a string array then then the contents of that array are added to yet another array
+                ArrayList<String> currentdatafeed = new ArrayList<String>();
+                ArrayList<String> itemstoadd = new ArrayList<String>();
+                itemstoadd.add(date);
+                itemstoadd.add(time);
+                itemstoadd.add(winddirection);
+                itemstoadd.add(windspeed);
+                itemstoadd.add(waveheight);
+                itemstoadd.add(waveinterval);
+                itemstoadd.add(winddirectiondegrees);
+                currentdatafeed.addAll(itemstoadd);
+
+                tb.putList(recordKey, currentdatafeed);
+
+                currentdatafeed.clear();
+                itemstoadd.clear();
+
+
                 int recordNumber = 7;//number of records to be scrapped and populated in the database. Skips the first two tr.
-                String recordKey = "saveddatarecord";
-                String date;
-                String time;
-                String winddirection;
-                String windspeed;
-                String waveheight;
-                String waveinterval;
-                String winddirectiondegrees;
 
                 for (Element table : doc.select("table.dataTable")) {//main loop to go through the source web page
 
@@ -82,121 +156,14 @@ public class JsoupWebScrape {
                             String demilms = "[ ]";
                             String[] tokens = cleanedRow.split(demilms);
 
-                            int month = Integer.valueOf(tokens[0]);// index 0 element in the token array is the month.
+                            month = Integer.valueOf(tokens[0]);// index 0 element in the token array is the month.
 
-                            String nameOfMonth = null;
-
-                            switch (month) {
-                                case 1:
-                                    nameOfMonth = "January";
-                                    break;
-                                case 2:
-                                    nameOfMonth = "February";
-                                    break;
-                                case 3:
-                                    nameOfMonth = "March";
-                                    break;
-                                case 4:
-                                    nameOfMonth = "April";
-                                    break;
-                                case 5:
-                                    nameOfMonth = "May";
-                                    break;
-                                case 6:
-                                    nameOfMonth = "June";
-                                    break;
-                                case 7:
-                                    nameOfMonth = "July";
-                                    break;
-                                case 8:
-                                    nameOfMonth = "August";
-                                    break;
-                                case 9:
-                                    nameOfMonth = "September";
-                                    break;
-                                case 10:
-                                    nameOfMonth = "October";
-                                    break;
-                                case 11:
-                                    nameOfMonth = "November";
-                                    break;
-                                case 12:
-                                    nameOfMonth = "December";
-                                    break;
-                                default:
-                                    Log.d(TAG, "Invalid value for month");
-                                    break;
-                            }
+                             nameOfMonth = getNameOfMonth(month);
 
                             String windDirection = tokens[3];//index 3 in the token array represents the wind direction.
-                            winddirectiondegrees = null;
+                            winddirectiondegrees = getWindDirectionInDegrees(windDirection);
 
-                            switch (windDirection) {//the wind direction is used to determine the wind direction in degrees.
-                                case "N":
-                                    winddirectiondegrees = "360";
-                                    break;
-                                case "NNE":
-                                    winddirectiondegrees = "22";
-                                    break;
-                                case "NE":
-                                    winddirectiondegrees = "45";
-                                    break;
-                                case "ENE":
-                                    winddirectiondegrees = "67";
-                                    break;
-                                case "E":
-                                    winddirectiondegrees = "90";
-                                    break;
-                                case "ESE":
-                                    winddirectiondegrees = "112";
-                                    break;
-                                case "SE":
-                                    winddirectiondegrees = "135";
-                                    break;
-                                case "SSE":
-                                    winddirectiondegrees = "157";
-                                    break;
-                                case "S":
-                                    winddirectiondegrees = "180";
-                                    break;
-                                case "SSW":
-                                    winddirectiondegrees = "202";
-                                    break;
-                                case "SW":
-                                    winddirectiondegrees = "225";
-                                    break;
-                                case "WSW":
-                                    winddirectiondegrees = "247";
-                                    break;
-                                case "W":
-                                    winddirectiondegrees = "270";
-                                    break;
-                                case "WNW":
-                                    winddirectiondegrees = "292";
-                                    break;
-                                case "NW":
-                                    winddirectiondegrees = "315";
-                                    break;
-                                case "NNW":
-                                    winddirectiondegrees = "337";
-                                    break;
-                                default:
-                                    Log.d(TAG, "Invalid wind direction");
-                                    break;
-                            }
-
-                            time = tokens[2];//index 2 in the token array represents the report time.
-                            String[] hour = time.split(":");
-                            int h = Integer.parseInt(hour[0]);//get the hour of time from the token
-
-                            if (time.contains("am") && (h == 12)) {//change to 24 hour clock if time is 12 am.
-                                time = "00:00 am";
-                            }
-
-                            if (time.contains("pm") && h < 12) {//change to 24 hour clock
-                                h = h + 12;
-                                time = h + ":00 pm";
-                            }
+                            time = setReportTime(tokens[2]);
 
                             Calendar cal;//used to get the year
                             cal = Calendar.getInstance();
@@ -217,8 +184,8 @@ public class JsoupWebScrape {
                             }
 
                             //the Halibut bank data items are added to a string array then then the contents of that array are added to yet another array
-                            ArrayList<String> currentdatafeed = new ArrayList<String>();
-                            ArrayList<String> itemstoadd = new ArrayList<String>();
+                            currentdatafeed.clear();
+                            itemstoadd.clear();
                             itemstoadd.add(date);
                             itemstoadd.add(time);
                             itemstoadd.add(winddirection);
@@ -228,26 +195,7 @@ public class JsoupWebScrape {
                             itemstoadd.add(winddirectiondegrees);
                             currentdatafeed.addAll(itemstoadd);
 
-                            switch (recordNumber) {
-                                case 1:
-                                    recordKey = "saveddatarecord1";
-                                    break;
-                                case 2:
-                                    recordKey = "saveddatarecord2";
-                                    break;
-                                case 3:
-                                    recordKey = "saveddatarecord3";
-                                    break;
-                                case 4:
-                                    recordKey = "saveddatarecord4";
-                                    break;
-                                case 5:
-                                    recordKey = "saveddatarecord5";
-                                    break;
-                                case 6:
-                                    recordKey = "saveddatarecord6";
-                                    break;
-                            }
+                            recordKey = setRecordKey(recordNumber);
 
                             tb.putList(recordKey, currentdatafeed);
 
@@ -259,9 +207,9 @@ public class JsoupWebScrape {
                     }//end of the inner for range loop going through the table data.
                 }//end of for range loop through full doc
 
-                tb.putInt("recordssaved", 5);//for 5 records scrapped from the web.
+                tb.putInt("recordssaved", 6);//for 5 records scrapped from the web.
 
-                ArrayList<String> lastRecord = tb.getList("saveddatarecord5");
+                ArrayList<String> lastRecord = tb.getList("saveddatarecord6");
                 String hourString = lastRecord.get(1);
                 String[] hour = hourString.split(":");
                 int h = Integer.parseInt(hour[0]);//get the hour of time from the token
@@ -355,4 +303,149 @@ public class JsoupWebScrape {
 
         }//end of webscrape while loop
     }//end of scrapeHalibutBankData method.
+
+    private String setReportTime(String time) {
+        String[] hour = time.split(":");
+        int h = Integer.parseInt(hour[0]);//get the hour of time from the token
+
+        if (time.contains("am") && (h == 12)) {//change to 24 hour clock if time is 12 am.
+            time = "00:00 am";
+        }
+
+        if (time.contains("pm") && h < 12) {//change to 24 hour clock
+            h = h + 12;
+            time = h + ":00 pm";
+        }
+        return time;
+    }
+
+    private String setRecordKey(int recordNumber) {
+        String recordKey = null;
+        switch (recordNumber) {
+            case 1:
+                recordKey = "saveddatarecord1";
+                break;
+            case 2:
+                recordKey = "saveddatarecord2";
+                break;
+            case 3:
+                recordKey = "saveddatarecord3";
+                break;
+            case 4:
+                recordKey = "saveddatarecord4";
+                break;
+            case 5:
+                recordKey = "saveddatarecord5";
+                break;
+            case 6:
+                recordKey = "saveddatarecord6";
+                break;
+        }
+        return recordKey;
+    }
+
+    private String getWindDirectionInDegrees(String windDirection) {
+        String winddirectiondegrees = null;
+        switch (windDirection) {//the wind direction is used to determine the wind direction in degrees.
+
+            case "N":
+                winddirectiondegrees = "360";
+                break;
+            case "NNE":
+                winddirectiondegrees = "22";
+                break;
+            case "NE":
+                winddirectiondegrees = "45";
+                break;
+            case "ENE":
+                winddirectiondegrees = "67";
+                break;
+            case "E":
+                winddirectiondegrees = "90";
+                break;
+            case "ESE":
+                winddirectiondegrees = "112";
+                break;
+            case "SE":
+                winddirectiondegrees = "135";
+                break;
+            case "SSE":
+                winddirectiondegrees = "157";
+                break;
+            case "S":
+                winddirectiondegrees = "180";
+                break;
+            case "SSW":
+                winddirectiondegrees = "202";
+                break;
+            case "SW":
+                winddirectiondegrees = "225";
+                break;
+            case "WSW":
+                winddirectiondegrees = "247";
+                break;
+            case "W":
+                winddirectiondegrees = "270";
+                break;
+            case "WNW":
+                winddirectiondegrees = "292";
+                break;
+            case "NW":
+                winddirectiondegrees = "315";
+                break;
+            case "NNW":
+                winddirectiondegrees = "337";
+                break;
+            default:
+                Log.d(TAG, "Invalid wind direction");
+                break;
+        }
+        return winddirectiondegrees;
+    }
+
+    private String getNameOfMonth (int month){
+        String nameOfMonth = null;
+        switch (month) {
+            case 1:
+                nameOfMonth = "January";
+                break;
+            case 2:
+                nameOfMonth = "February";
+                break;
+            case 3:
+                nameOfMonth = "March";
+                break;
+            case 4:
+                nameOfMonth = "April";
+                break;
+            case 5:
+                nameOfMonth = "May";
+                break;
+            case 6:
+                nameOfMonth = "June";
+                break;
+            case 7:
+                nameOfMonth = "July";
+                break;
+            case 8:
+                nameOfMonth = "August";
+                break;
+            case 9:
+                nameOfMonth = "September";
+                break;
+            case 10:
+                nameOfMonth = "October";
+                break;
+            case 11:
+                nameOfMonth = "November";
+                break;
+            case 12:
+                nameOfMonth = "December";
+                break;
+            default:
+                Log.d(TAG, "Invalid value for month");
+                break;
+        }
+        return nameOfMonth;
+    }
 }//end of JsoupWebScrape class
