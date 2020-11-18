e60d7aa824c98796ae5347a1e477992aaa47987f
Scott Norrena <snorrena@gmail.com>
2020-11-17 18:37:09 -0800

    added code to webcam to keep in webview and prevent from opening in browser

--- app/src/main/java/com/rsnorrena/westvansurfreport/WebCam.java
+++ app/src/main/java/com/rsnorrena/westvansurfreport/WebCam.java
@@ -1,22 +1,48 @@
 package com.rsnorrena.westvansurfreport;
 
 import android.app.Activity;
+import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.KeyEvent;
+import android.webkit.WebSettings;
 import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 public class WebCam extends Activity {
 
     WebView wv;
+    String url = "http://www.kiteboardbc.com/webcam/westvan/";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.webcam);
         wv = (WebView) findViewById(R.id.webView);
+        wv.requestFocus();
         wv.getSettings().setLoadWithOverviewMode(true);
         wv.getSettings().setUseWideViewPort(true);
-        wv.loadUrl("http://www.kiteboardbc.com/webcam/westvan/");
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            wv.getSettings().setMixedContentMode(WebSettings.MIXED_CONTENT_COMPATIBILITY_MODE);
+        }
+        wv.loadUrl(url);
+        wv.setWebViewClient(new WebViewClient(){
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                view.loadUrl(url);
+                return true;
+            }
+        });
+    }
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if ((keyCode == KeyEvent.KEYCODE_BACK) && wv.canGoBack()){
+            wv.goBack();
+        }else{
+            finish();
+        }
+        return super.onKeyDown(keyCode, event);
     }
 
     @Override
@@ -30,7 +56,7 @@ public class WebCam extends Activity {
     protected void onResume() {
         super.onResume();
         Log.d("onResume", "called");
-        wv.loadUrl("http://www.kiteboardbc.com/webcam/westvan/");
+        wv.loadUrl(url);
 
     }
 }
